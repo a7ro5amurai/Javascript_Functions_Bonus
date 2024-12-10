f35a560db627d465f43dfbf4d5cc14a03ce30847
a7ro5amurai <m.shuhait@outlook.de>
2024-12-10 14:40:41 +0100

    Javascript_Functions_Bonus

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const mainBackgroundColor = document.getElementById("changeMainBackgroundColor")
+
+const h1Color = document.getElementById("changeH1Color")
+
+function greenpurple() {
+    mainBackgroundColor.style.backgroundColor = "green";
+    h1Color.style.color = "purple";
+}
+
+function greyyellow() {
+    mainBackgroundColor.style.backgroundColor = "grey";
+    h1Color.style.color = "yellow";
+}
+
+function blackorange() {
+    mainBackgroundColor.style.backgroundColor = "black";
+    h1Color.style.color = "orange";
+}
+
+function darkbluelightblue() {
+    mainBackgroundColor.style.backgroundColor = "darkblue";
+    h1Color.style.color = "lightblue";
+}
